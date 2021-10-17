ef7887a18d5243c368fd2194125b09b1590309f8
Christopher-Simon <christopher.simon.wells@gmail.com>
2021-10-17 03:32:28 +0200

    pour etre sûr

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: chsimon <chsimon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/13 14:30:11 by chsimon           #+#    #+#             */
-/*   Updated: 2021/10/13 20:38:29 by chsimon          ###   ########.fr       */
+/*   Updated: 2021/10/14 18:07:16 by chsimon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,9 @@
 
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 int	ft_strlen(char *str);
+int	ft_strncmp(const char *s1, const char *s2, size_t n);
 
 #include <stdio.h>
-
-int	count_start(char const *s1, char const *set, int i )
-{
-	while (s1[i])
-	{
-		if (s1[i] == set[i])
-			i++;
-		else
-			
-	}
-}
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
@@ -39,7 +29,7 @@ char	*ft_strtrim(char const *s1, char const *set)
 	len = ft_strlen((char *)s1);
 	i = 0;
 	j = 2;	
-	count_start(s1, set, i);	
+	printf("%d\n", ft_strncmp(s1, set, 2));	
 	trim = ft_substr((char *)s1, i, len - i - j);
 	return (trim);
 }
