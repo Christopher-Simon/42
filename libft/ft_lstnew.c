06abae5092fad7217d61209c5b6f89fc008468f9
Christopher SIMON <chsimon@e2r3p6.clusters.42paris.fr>
2021-11-22 18:11:32 +0100

    1er jour, on avance sur les listes

--- libft/ft_lstnew.c
+++ libft/ft_lstnew.c
@@ -6,7 +6,7 @@
 /*   By: chsimon <chsimon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/15 19:08:35 by chsimon           #+#    #+#             */
-/*   Updated: 2021/11/19 14:00:04 by chsimon          ###   ########.fr       */
+/*   Updated: 2021/11/22 18:10:59 by chsimon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,11 @@
 */
 typedef struct t_list
 {
-	int		x;
+	void		*x;
 	struct t_list	*next;
 } t_list;
 
-t_list	*ft_lstnew(int content)
+t_list	*ft_lstnew(void	*content)
 {
 	t_list *root;
 
@@ -35,17 +35,16 @@ void	ft_print_lst(t_list *node)
 {
 	while (node != NULL)
 	{
-		printf("%d", node->x);
+		printf("%p->", node->x);
 		node = node->next;
 	}
+	printf("NULL\n");
 }
 
 void	ft_lstadd_front(t_list **alst, t_list *new)
 {
-	t_list	*tmp;
-	tmp = malloc(sizeof(t_list));
-	tmp->x = new;
-	tmp->next = *alst;	
+	new->next = *alst;	
+	*alst = new;
 }
 
 int	ft_lstsize(t_list *lst)
@@ -63,34 +62,45 @@ int	ft_lstsize(t_list *lst)
 
 void	ft_lstadd_back(t_list **alst, t_list *new)
 {
-	t_list	*tmp;
-
-	tmp = malloc(sizeof(t_list));
-	tmp->x = new;
-	tmp->next = NULL;
-	if (alst == NULL)
-	{
-		alst = tmp;
-	}
-	while (alst->next != NULL)
-		alst = alst->next;
-	alst = tmp;
+	t_list	*mem;
+	mem = *alst;
+	new->next = NULL;
+	while ((*alst)->next != NULL)
+		*alst = (*alst)->next;
+	(*alst)->next = new;	
+	*alst = mem;
 }
 
-
+/*
 t_list	*ft_lstlast(t_list *lst)
 {
 	while(lst->next != NULL)
 		lst = lst->next;
+	return (lst);
 }
-
+*/
 int	main(void)
 {
-	int	x = 1;	
-	t_list *list;
+	int	x = 1;
+	int	y = 5;
+	int	z = 7;
+	t_list	*list;
+	t_list	*new_list;
 
-	list = ft_lstnew(x);
-	//ft_lstadd_front(list, new);
-	ft_print_lst(list);		
+	void	*p = &x;
+	printf("create a list\n");
+	list = ft_lstnew(p);
+	ft_print_lst(list);
+	printf("add to front\n");
+	p = &y;
+	new_list = ft_lstnew(p);	
+	ft_lstadd_front(&list, new_list);
+	ft_print_lst(list);
+	printf("size : %d\n", ft_lstsize(list));
+	printf("add to back\n");
+	p = &z;
+	new_list = ft_lstnew(p);	
+	ft_lstadd_back(&list, new_list);
+	ft_print_lst(list);
 	return (0);
 }
