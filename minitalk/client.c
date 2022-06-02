59c375d6bab86812429f2c2dc3619d636d52523a
Christopher <christopher.simon.wells@gmail.com>
2022-06-02 03:07:00 +0200

    ok

--- minitalk/client.c
+++ minitalk/client.c
@@ -6,7 +6,7 @@
 /*   By: chsimon <chsimon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/01 10:59:40 by chsimon           #+#    #+#             */
-/*   Updated: 2022/06/01 20:50:08 by chsimon          ###   ########.fr       */
+/*   Updated: 2022/06/02 03:03:13 by chsimon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,6 @@ t_global	g_test;
 // 	t_char	b;
 
 // 	b->b >> i = a;
-b >>= 1;
 // 	b->i--;
 // }
 
@@ -40,31 +39,56 @@ b >>= 1;
 // 	return (1);
 // }
 
-// void	sighandler(int signum)
-// {
-// 	(void)signum;
-// 	ft_printf("C'est fini, j'envoie la suite\n");
-// 	test_acc_recep(g_test.serv_pid);
-// }
 
-int	test_acc_recep(void)
+int	test(int a)
 {
-	while (g_test.i-- > 0)
-	{
+	ft_printf("_______________TEST________________\n");
+	ft_putnbr_base(a, "01");
+	ft_printf("\n%d\n", a);
+	// a >>= 1;
+	g_test.i--;
+	ft_printf("i : %d\n", g_test.i);
+	ft_printf("%d", (a >> g_test.i) & 1);
+	if (((a >> g_test.i) & 1))
 		kill(g_test.serv_pid, SIGUSR1);
-		sleep(1);
-	}
-	// signal(SIGUSR1, sighandler);
+	if (!((a >> g_test.i) & 1))
+		kill(g_test.serv_pid, SIGUSR2);
+	ft_printf("\n%d\n", (a >> g_test.i) & 1);
+	ft_printf("\n");
+	// int	b;
+	
+	// b = 0;
+	// b |= (((a >> i) & 1) << i);
+	// i = 16;
+	// while (i-- > 0)
+	// 	ft_printf("%d", (b >> i) & 1);
 	return (1);
+}
+
+void	sighandler(int signum)
+{
+	if (signum == SIGUSR1) 
+		test(253);
+	if (signum == SIGUSR2)
+		exit (1);
+
 }
 
 int	main(int argc, char **argv)
 {
 	(void)argc;
-	g_test.i = 3;
+	if (argc <= 1)
+		return (0);
+	ft_putnbr_base(2147483647, "01");
+	ft_printf("\n%d\n", getpid());
+	g_test.i = 16;
 	g_test.serv_pid = ft_atoi(argv[1]);
-	test_acc_recep();
+	signal(SIGUSR1, sighandler);
+	signal(SIGUSR2, sighandler);
+	test(253);
+	while (1)
+		sleep(1);
 	return (1);
 }
-
+	// 1111 1111 1111 1111 1111 1111 1111 111
 	// kill(ft_atoi(argv[1]), SIGUSR1);
--- minitalk/server.c
+++ minitalk/server.c
@@ -6,22 +6,55 @@
 /*   By: chsimon <chsimon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/01 10:59:45 by chsimon           #+#    #+#             */
-/*   Updated: 2022/06/01 21:14:26 by chsimon          ###   ########.fr       */
+/*   Updated: 2022/06/02 03:06:33 by chsimon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_minitalk.h"
 
-typedef struct sigaction
+typedef struct g_global
 {
-	void	(*sa_handler)(int);
-	void
-}
+	int	b;
+	int	i;
+}	t_global;
 
-void	sighandler(int signum)
+	t_global g_serv;
+
+
+void	sighandler(int signum, siginfo_t *info, void *ucontext_t)
 {
-	(void)signum;
-	write(1, "1\n", 2);
+	int	b;
+
+	(void)ucontext_t;
+	b = 0;
+	g_serv.i--;
+	// ft_putnbr_fd(g_serv.i, 1);
+	if (g_serv.i >= 0)
+	{
+		// ft_putstr_fd("in", 1);
+		// ft_putstr_fd("\n", 1);
+		if (signum == SIGUSR1)
+		{
+			write(1,"1",1);
+			b |= (1 << g_serv.i);
+		}
+		if (signum == SIGUSR2)
+		{
+			write(1,"0",1);
+			b |= (0 << g_serv.i);	
+		}
+		kill(info->si_pid, SIGUSR1);
+	}
+	else
+	{
+		kill(info->si_pid, SIGUSR2);
+			int	i = 16;
+		ft_putstr_fd("\n", 1);
+		while (i-- > 0)
+			ft_printf("%d", (b >> i) & 1);
+		ft_putstr_fd("\n", 1);
+		ft_printf("%d", b);
+	}
 }
 
 // void	bit_shift_left(int *tab)
@@ -40,15 +73,20 @@ void	sighandler(int signum)
 // 	i++;
 // 	ft_printf("a : %d\n", a);
 // }
-
 int	main(int argc, char **argv)
 {
+	struct sigaction	sa;
+
+	sa.sa_sigaction = &sighandler;
+	sa.sa_flags = SA_SIGINFO;
+	g_serv.i = 16;
+	g_serv.b = 0;
 	(void)argc;
 	(void)argv;
-
+	sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR2, &sa, NULL);
 	ft_printf("%d\n", getpid());
-	signal(SIGUSR1, sighandler);
-	sigaction(SIGUSR1, ,NULL);
+	// signal(SIGUSR1, sighandler);
 	while (1)
 		sleep(1);
 	return (1);
